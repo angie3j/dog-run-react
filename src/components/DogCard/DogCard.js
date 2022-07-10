74f85ffb77c56994a28659211dd849b985ed9bfe
Angie Guerrero <angieg@Angies-MBP.fios-router.home>
2022-07-10 12:29:11 -0400

    Add Dog Card Component

---
+++ src/components/DogCard/DogCard.js
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+
+import './DogCard.scss';
+
+const DogCard = ({DogCard}) => {
+
+    // props deconstructed
+    const { dogPic, dogsName, ownersFirstName, ownersLastName, email, telephone } = dog;
+
+    // hooks
+
+    return (
+        <div className='dogCard'>
+            <Link to={`/dogs/${dog.id}`} state={{ dog:dog }}>
+            <div className='dogCard__dogPic'>
+                <img src={dogPic} />
+            </div>
+
+            <div className='dogCard__info'>
+
+                <div className='dogCard__infoLine'>
+                   Dog's Name: {dogsName}
+                </div>
+                
+                <div className='dogCard__name'>
+                    {`${ownersFirstName} ${ownersLastName}`}
+                </div>
+                
+                <div className='dogCard__infoLine'>
+                    Email: {email}
+                </div>
+
+                <div className='dogCard__infoLine'>
+                    Telephone: {telephone}
+                </div>
+
+            </div>
+            </Link>
+
+        </div>
+    )
+
+}
+
+
+export default DogCard;
